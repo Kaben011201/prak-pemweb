5ede2d4d5b7f0afdca422f6f67b86ee0ee9a115a
Bendry <bendrylakburlawal01@gmail.com>
2023-11-04 14:45:50 +0700

    menambah komen

--- prak4/121140111_BendryLakburlawal_Prak4.js
+++ prak4/121140111_BendryLakburlawal_Prak4.js
@@ -1,16 +1,17 @@
 //by Bendry Lakburlawal - 121140111
 //Praktikum Pemrograman Web RB - Tugas 4
 
-let bilangan=90;
-let ganjil=0;
-let genap=0;
+let bilangan=90; //inisialisasi variabel bilangan dengan value 90
+                //sebagai bilangan yang akan dicek nilai ganjil dan genapnya.
+let ganjil=0; //buat nilai ganjil awal adalah 0
+let genap=0;   //buat nilai genap awal adalah 0
 
-for(let i=1; i<=bilangan; i++){
-    if(i%2==0){
+for(let i=1; i<=bilangan; i++){ //perulangan untuk mengecek dan menjumlahkan nilai ganjil genap yang ada pada angka 90
+    if(i%2==0){ //untuk mengcek angka yang habis dibagi 2
         genap++;
-    }else{
+    }else{ //untuk mengecek angka yang tidak habis dibagi 2
         ganjil++;
     }
 }
-console.log(`Bilangan Ganjil: ${ganjil}\nBilangan Genap: ${genap}`);
-alert(`Bilangan Ganjil: ${ganjil}\nBilangan Genap: ${genap}`);
+console.log(`Bilangan Ganjil: ${ganjil}\nBilangan Genap: ${genap}`); //untuk menampilkan hasil pengecekan pada console
+alert(`Bilangan Ganjil: ${ganjil}\nBilangan Genap: ${genap}`); //untuk pop up hasil pengecekan pada window.
--- prak5/121140111_BendryLakburlawal_Prak5.js
+++ prak5/121140111_BendryLakburlawal_Prak5.js
@@ -1,28 +1,35 @@
 // by Bendry Lakburlawal - 121140111
 // Praktikum Pemrograman Web RB - Tugas 5
 
+// Inisialisasi variabel calcu dengan value ''.
 let calcu = '';
-let resultOfCal = document.querySelector('.js-result');
-let zero = document.querySelector('.js');
-let cek = false;
 
-function updateCalculation(event){
-    if(cek == true){
+//inisialisasi variabel resultOfCal menggunankan DOM yang terkoneksi dengan class js-result
+let resultOfCal = document.querySelector('.js-result');
+let cek = false; //inisialisasi variabel cek dengan value false untuk mengecek hasil perhitungan yang bernilai 0
+
+//fungsi ini digunakan untuk menulis nilai yang mau dihitung pada kalkulator
+function updateCalculation(event){ //event adalah parameter yang akan memberikan nilai yang ingin dihitung
+                                    // seperti 1, 2, 3, +, -, dst.
+    if(cek == true){ //untuk mengecek ketika nilai cek adalah true berarti nilai calcu adalah 0
+                     // maka nilai calcu diubah kembali menjadi '' dan cek diubah menjadi false.
         calcu = '';
         cek = false;
     }
-    calcu += event;
-    resultOfCal.innerHTML = `${calcu}`;
+    calcu += event; // untuk menambah angka-angka yang mau dihitung
+    resultOfCal.innerHTML = `${calcu}`; //menampilkan angka2 yang akan dihitung
 }
 
-function result(){
-    calcu= eval(calcu);
-    if(calcu == '0'){
+//fungsi ini untuk menghitung nilai-nilai yang dimasukkan.
+function result(){ 
+    calcu= eval(calcu); //eval adalah fungsi yang melakukan operasi perhitungan
+    if(calcu == '0'){   //ketika hasil perhitungan adalah 0, nilai cek diubah menjadi true.
         cek = true;
     }
-    resultOfCal.innerHTML = `${calcu}`;
+    resultOfCal.innerHTML = `${calcu}`; //menampilkan hasil perhitungan
 }
 
+//fungsi ini untuk menghapus angka-angka hasil perhitungan yang ditampilkan.
 function clearNum(){
     calcu='';
     resultOfCal.innerHTML = `${'0'}`;
